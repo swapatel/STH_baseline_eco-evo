a1c67016449359e6714fdd47f3bf041613fef5f4
swapatel <82217629+swapatel@users.noreply.github.com>
2023-12-15 10:40:35 -0800

    Update supplement_figure_evolution.R

--- supplement_figure_evolution.R
+++ supplement_figure_evolution.R
@@ -1,27 +1,9 @@
 # Supplementary Figures- evolution
-
-odefn <- function(t, n, parms) {
-  with(as.list(c(parms, n)), {
-    dW = beta_eff*N*L - (mu_h + mu_a)*W
-    dL = 1/2 *W *lambda_eff - (mu_l + beta_eff*N)*L
-    dx = beta_eff*N*L*(y-x)/W
-    dy = 1/2*lambda_eff*W *(x-y)/L    
-    return(list(c(dW,dL,dx,dy))) 
-  })
-}
-
-# treatment function
-treatfun <- function(t, y, parms){
-  with(as.list(parms, y), {
-    x <- y[3]
-    mubar_MDA <- (mu_AA*x^2 + mu_AB*2*x*(1-x) + mu_BB*(1-x)^2)
-    mubar_A <- (mu_AA*x + mu_AB*(1-x))  # marginal mortality probability of allele A
-    y[1] <- y[1]*(1-cov*mubar_MDA)
-    y[3] <- y[3]*(1-cov*mubar_A)/(1-cov*mubar_MDA)
-    return(y)
-  })
-}
-
+library(deSolve)
+# load parameter_for_species file
+source("~/parameters_for_species.R")
+# load model.R file 
+source("~/model.R")
 
 solve_ode.beta <- function(initial_cond, times, treat.times, parameters){
   R0 <- c(1,5,10)
